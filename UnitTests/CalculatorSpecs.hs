cd7f337e7c8f555959a0e5909b3ce7baf7718f05
Krzysztof Moczała <krzysztof.moczala@gmail.com>
2016-01-08 17:40:44 +0100

    Confirm that calculator can calculate correct answer when input is multi digit number

--- UnitTests/CalculatorSpecs.hs
+++ UnitTests/CalculatorSpecs.hs
@@ -5,7 +5,9 @@ import Calculator
 
 testCalculator :: IO ()
 testCalculator = hspec $ 
-    describe "calculate" $ 
+    describe "calculate" $ do
         it "1=1" $ 
             calculate "1" `shouldBe` (1::Double)
+        it "10=10" $
+            calculate "10" `shouldBe` (10::Double)
             
